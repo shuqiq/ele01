eef1669299680442f7f27939d98e03112168c538
sun <2856811609@qq.com>
2018-01-17 14:45:38 +0800

    第一次提交

---
+++ ele-app/src/common/Page.vue
@@ -0,0 +1,36 @@
+<template>
+<div class="page" :id="pageId" ref="page">
+<div>
+<slot></slot>
+</div>
+
+</div>
+</template>
+
+<script>
+import IScroll  from 'iscroll'
+export default{
+    name:'page',
+   props: {
+       pageId:String
+   },
+   mounted(){
+       let pageScroll = new IScroll(this.$refs.page, {
+
+       });
+   }
+
+}    
+
+</script>
+
+<style>
+.page{
+  width:100%; 
+  position:absolute;
+  top:0;
+  left:0;
+  bottom: 49px;
+  overflow: hidden;
+}
+</style>
---
+++ ele-app/src/components/home/index/Header.vue
@@ -0,0 +1,23 @@
+<template>
+<header class="header">
+  <div>福中三路深圳市民中心</div>
+  <div>商家搜索</div>
+</header>
+
+</template>
+
+<script>
+
+export default{
+   name: 'home-header'
+
+}
+</script>
+
+<style scoped>
+.header{
+    width:100%; header:80px;
+    background-image: linear-gradient(90deg,#0af,#0085ff);
+
+}
+</style>
